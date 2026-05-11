4d9e0b2bc21a0a6d2ebf74aea06ef4dbf2b7d351
STEMAlexandraMorales <thealexandra.i.morales@gmail.com>
2026-05-11 09:48:51 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,33 @@
 import nltk
+import os
 from nltk.corpus import wordnet as wn
-try:
-    wn.synsets('metal')
-except LookupError:
-    nltk.download('wordnet')
 
-def get_semantic_synonyms(word):
-    """
-    Uses WordNet to find a set of synonyms for a given word.
-    This allows the game to be more flexible (e.g., accepting 'petrol' for 'petroleum').
-    """
-    synonyms = {word.lower()}
-    for syn in wn.synsets(word):
-        for lemma in syn.lemmas():
-            synonyms.add(lemma.name().lower().replace('_', ' '))
-    return synonyms
+nltk_data_path = "/tmp/nltk_data"
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+nltk.data.path.append(nltk_data_path)
+
+# Only download if not already present in the tmp folder
+try:
+    wn.ensure_loaded()
+except:
+    nltk.download('wordnet', download_dir=nltk_data_path)
+    nltk.download('omw-1.4', download_dir=nltk_data_path)
 
 def check_semantic_match(input_a, input_b, recipes):
-    """
-    Checks if the user's input matches any recipe keys, 
-    even if they use a synonym found in WordNet.
-    """
-    syns_a = get_semantic_synonyms(input_a)
-    syns_b = get_semantic_synonyms(input_b)
+    syns_a = {input_a.lower()}
+    syns_b = {input_b.lower()}
     
-    for recipe_pair in recipes.keys():
-        # recipe_pair is a sorted tuple of lowercase strings
-        target_a, target_b = recipe_pair
-        
-        # Check if user syns for A match target A and user syns for B match target B
-        # Or vice versa
-        if (target_a in syns_a and target_b in syns_b) or            (target_a in syns_b and target_b in syns_a):
-            return recipes[recipe_pair]
-            
-    return None
+    for syn in wn.synsets(input_a):
+        for lemma in syn.lemmas():
+            syns_a.add(lemma.name().lower().replace('_', ' '))
+    for syn in wn.synsets(input_b):
+        for lemma in syn.lemmas():
+            syns_b.add(lemma.name().lower().replace('_', ' '))
 
-def format_score_bar(innovation, degradation, length=20):
-    """
-    Returns a visual string representation of the stability balance.
-    """
-    total = innovation + degradation
-    if total == 0:
-        return "[" + "-" * length + "]"
-    
-    inn_part = int((innovation / total) * length)
-    deg_part = length - inn_part
-    return "[" + "=" * inn_part + "!" * deg_part + "]"
+    for recipe_pair in recipes.keys():
+        target_a, target_b = recipe_pair
+        if (target_a in syns_a and target_b in syns_b) or \
+           (target_a in syns_b and target_b in syns_a):
+            return recipes[recipe_pair]
+    return None
